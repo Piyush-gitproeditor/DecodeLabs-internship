3d6f2ae8e4ef670dca88830113e1491a63ecd660
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-09-01 19:26:52 +0530

    Remove duplicate folders after renaming

--- Rule Based ChatBot/file.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-file_path = Path(__file__).with_name("file.txt")
-
-if file_path.exists():
-    print("\n FILE FOUND!\n")
-
-with file_path.open(encoding="utf-8") as f:
-    print(f.read())

f01b1cc6e0b108ede7d98a0f30e242665972c2bb
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-09-14 22:45:36 +0530

    happy coding

--- AI_Image_text_recognition-piyush saini/main.py
+++ AI_Image_text_recognition-piyush saini/main.py
@@ -13,7 +13,28 @@ print("Image loaded successfully!")
 print("Image size:", image.shape)
 
 # Perform OCR
-text = pytesseract.image_to_string(image)
+#text = pytesseract.image_to_string(image)
+data= pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+print("\n============OCR RESULTS============\n")
+t_confidence=0
+count =0
+for i in range(len(data["text"])):
+    word= data["text"][i].strip()
+    confidence = float(data["conf"][i])
+
+    if word != "" and confidence >=0:
+        print(word," confidence:",confidence)
+        t_confidence += confidence
+        count +=1
+
+if count>0:
+    avg_confidence = t_confidence/count
+    print("\n Average confidence:", round(avg_confidence,2),"%")
+
+    if avg_confidence>=80:
+        print("Status: Pass")
+    else:
+        print("Status : Below 80%")
 
 # Display the recognized text
 print("\n========== RECOGNIZED TEXT ==========\n")
